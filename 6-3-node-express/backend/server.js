af8d29692c073fcdec3d7b591dada63c2fe9972e
WujudAhmed <s202279120@kfupm.edu.sa>
2026-04-06 11:53:11 +0300

    ToDo 4&5

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -6,9 +6,12 @@ const app = express();
 const PORT = 3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
+import morgan from "morgan";
+app.use(morgan("dev")); 
 
 
 // TODO 6.1: Create root route "/"
